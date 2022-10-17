6aef78c3a52f6eacdbf6df14e76b3ddce41f8e40
emrekaankocoglu <emrekaankocoglu@gmail.com>
2022-10-17 19:04:44 +0000

    initial changes

--- sample_backend.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify,request,abort
-app=Flask(__name__,static_url_path='',static_folder='')
-@app.route('/session')
-def health():
-    return jsonify(host="164.92.227.51",port=6080)
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
